f9ab0d791f789636588e2d436ca372b78210d1cb
ashok-cm <ashokraja.cm@gmail.com>
2023-05-16 15:44:12 +0530

    Open in code editor support

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
 echo "Welcome to OCI Code Editor Samples"
+
+while true; do
+    read -p "Do you wish to proceed? " yn
+    case $yn in
+        [Yy]* ) echo "Yes, Done"; break;;
+        [Nn]* ) echo "Skipped"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
