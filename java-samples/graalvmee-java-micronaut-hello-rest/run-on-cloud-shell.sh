d29f7e9d7946718226ba1f2eaaf71c8085e0e027
ashok-cm <ashokraja.cm@gmail.com>
2023-06-09 16:59:29 +0530

    Update run-on-cloud-shell.sh

--- java-samples/graalvmee-java-micronaut-hello-rest/run-on-cloud-shell.sh
+++ java-samples/graalvmee-java-micronaut-hello-rest/run-on-cloud-shell.sh
@@ -9,6 +9,11 @@ echo "App is running in background"
 echo "Making curl to invoke endpoints"
 curl http://localhost:8080/Micronaut
 
+echo ""
 echo "Successfully completed"
-echo "Note: Still the service is running in background. Please run 'fg' and press Ctrl+C to kill the process"
+
+fg
+sleep 2
+
+echo "Note: Still the service is running, press Ctrl+C to kill the process"
 echo "Thanks for trying out this sample"
